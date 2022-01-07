26b67772533f28ca9449c4f873e7c5e9edfc62f4
Shem Maleriado <shem.maleriado@gmail.com>
2022-01-08 03:05:10 +0800

    feat: styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import synth from 'synth-js';
-import UploadComponent from './UploadComponent';
+import synth from 'synth-js'
+import UploadComponent from './UploadComponent'
 
-import compile from './scripts/b2aCompiler.js';
+import compile from './scripts/b2aCompiler.js'
 
 function App() {
   const [value, setValue] = useState('')
   const [showAudio, setShowAudio] = useState(false)
+  const [output, setOutput] = useState('')
 
   // Creates Brainfuck.mid and Brainfuck.wav under scripts/sounds
   function handleSubmit(event) {
@@ -29,7 +30,7 @@ function App() {
     //     audio.play();
 
     //     window.open(src);
-        
+
     //     // anchor.setAttribute('href', src);
     // });
 
@@ -38,43 +39,115 @@ function App() {
 
   return (
     <>
-      <header className="bg-gray-100 px-6 py-4 items-center justify-between">
-        <h1 className="text-red-500 font-medium text-lg">EARFUCK</h1>
+      <header className="items-center justify-between px-6 py-4 bg-gray-100">
+        <h1 className="text-xl font-medium text-red-500">EARFUCK</h1>
       </header>
-      <div className="flex flex-col gap-6 p-6 items-center flex-1">
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          <label htmlFor="instructions" className="font-medium text-gray-800">
-            Enter your instructions in brainfuck:
+      <div className="flex flex-1 gap-12 p-6">
+        <section className="flex flex-col gap-4 p-6 bg-gray-100 rounded sm:w-96">
+          <div>
+            <h2 className="text-xl font-medium text-red-500">
+              What is Brainfuck?
+            </h2>
+            <p className="text-gray-800">
+              Brainfuck is an esoteric programming language created in 1993 by
+              Urban Müller. Notable for its extreme minimalism, the language
+              consists of only eight simple commands, a data pointer and an
+              instruction pointer. While it is fully Turing complete, it is not
+              intended for practical use, but to challenge and amuse
+              programmers. Brainfuck simply requires one to break commands into
+              microscopic steps. The language's name is a reference to the slang
+              term brainfuck, which refers to things so complicated or unusual
+              that they exceed the limits of one's understanding.
+            </p>
+          </div>
+          <div>
+            <h2 className="text-xl font-medium text-red-500">
+              What is Earfuck?
+            </h2>
+            <p className="text-gray-800">Ur mom</p>
+          </div>
+          <div>
+            <h2 className="text-xl font-medium text-red-500">
+              How do I use this web app?
+            </h2>
+            <p className="text-gray-800">Ur mom</p>
+          </div>
+        </section>
+        <form onSubmit={handleSubmit} className="flex flex-col flex-1 gap-4">
+          <h2 className="text-xl font-medium text-red-500">
+            Brainfuck to Earfuck converter
+          </h2>
+          <label
+            htmlFor="instructions"
+            className="text-lg font-medium text-gray-800"
+          >
+            1. Input Brainfuck instructions
           </label>
-          <input
-            name="instructions"
-            type="text"
-            value={value}
-            onChange={(event) => setValue(event.target.value)}
-            className="px-4 py-2 bg-gray-100 text-gray-800 rounded"
-          />
-          <input
-            type="submit"
-            className="px-4 py-2 bg-red-500 text-white rounded font-medium hover:bg-red-600 transition-color"
-          />
+          <p className="text-gray-600">
+            To convert it to a MIDI file for download
+          </p>
+          <div className="flex gap-4">
+            <input
+              name="instructions"
+              type="text"
+              value={value}
+              onChange={(event) => setValue(event.target.value)}
+              className="flex-1 px-4 py-2 text-gray-800 bg-gray-100 rounded"
+            />
+            <input
+              type="submit"
+              className="px-4 py-2 font-medium text-white bg-red-500 rounded cursor-pointer hover:bg-red-600 transition-color"
+            />
+          </div>
+          <label className="text-lg font-medium text-gray-800">
+            2. Upload your MIDI file and listen!
+          </label>
+          <UploadComponent show={showAudio} />
         </form>
-        <UploadComponent show={showAudio} />
+        <form
+          className="flex flex-col flex-1 gap-4"
+          onSubmit={() => setOutput('ur mom')}
+        >
+          <h2 className="text-xl font-medium text-red-500">
+            Earfuck to Brainfuck converter
+          </h2>
+          <label htmlFor="upload" className="text-lg font-medium text-gray-800">
+            1. Upload a MIDI file
+          </label>
+          <div className="flex items-center justify-between gap-4">
+            <input type="file" />
+            <input
+              type="submit"
+              className="px-4 py-2 font-medium text-white transition-colors bg-red-500 rounded cursor-pointer hover:bg-red-600"
+            />
+          </div>
+          <h3 className="text-lg font-medium text-gray-800">
+            2. Brainfuck output
+          </h3>
+          <div className="p-6 text-gray-800 bg-gray-100 rounded">{output}</div>
+        </form>
       </div>
-      <footer className="flex bg-gray-100 p-6 sm:justify-between justify-end">
-        <p className="text-gray-400 hidden sm:block">
+      <footer className="flex justify-end p-6 bg-gray-100 sm:justify-between">
+        <p className="hidden text-gray-400 sm:block">
           Built by Gokul, Hyung Woon, Shem and Vignesh for Hack&Roll2022.
         </p>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="currentColor"
-          class="bi bi-github"
-          viewBox="0 0 16 16"
-          className="text-gray-400 flex-none hover:text-gray-500 cursor-pointer transition-colors"
+        <a
+          target="_blank"
+          href="https://github.com/vigneshsankariyer1234567890/earf__k"
+          rel="noreferrer"
+          className="cursor-pointer"
         >
-          <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-        </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+            className="flex-none text-gray-400 transition-colors cursor-pointer hover:text-gray-500"
+          >
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+          </svg>
+        </a>
       </footer>
     </>
   )
