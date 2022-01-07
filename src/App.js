7176889af3b8300cc8497d113b75f269b4719a04
Shem Maleriado <shem.maleriado@gmail.com>
2022-01-07 16:07:51 +0800

    refactor: ace editor

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
-import AceEditor from 'react-ace'
-
-function App() {
-  function onChange(newValue) {
-    console.log('change', newValue)
-  }
-
-  return (
-    <AceEditor
-      mode="java"
-      theme="github"
-      onChange={onChange}
-      name="UNIQUE_ID_OF_DIV"
-      editorProps={{ $blockScrolling: true }}
-    />
-  )
+function AppTest() {
+  return <div>hi</div>
 }
 
 export default App
