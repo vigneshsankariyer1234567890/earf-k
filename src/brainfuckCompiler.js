c6d2f4386bfd2d390661d3f9361a2f9f57ffc5b8
Vignesh Sankar Iyer <vignesh.sankar.iyer.1999@gmail.com>
2022-01-07 15:13:55 +0800

    compiler that returns a list of frequencies to be played

--- src/brainfuckCompiler.js
+++ src/brainfuckCompiler.js
@@ -1,14 +1,15 @@
 function compile(program) {
-    let tape       = Array(100).fill(0);
+    let tape       = Array(30000).fill(0);
     let ptr        = 0;
     let isLooping  = false;
     let loopStack  = [];
     let innerLoops = 0;
+    let frequencyList = [];
   
     for( i = 0; i < program.length; i++ ) {
   
-    const char = program[i];
-  
+      const char = program[i];
+    
       if(isLooping) {
         if(char === "[") innerLoops++;
           if(char === "]") {
@@ -21,36 +22,46 @@ function compile(program) {
       switch(char){
         case '+':
           tape[ptr]++;
+          frequencyList.push("G5");
           break;
         case '-':
           tape[ptr]--;
+          frequencyList.push("F5");
           break;
         case ',':
           tape[ptr] = prompt()[0].charCodeAt()
+          frequencyList.push("B5");
           break;
         case '.':
           console.log(String.fromCharCode(tape[ptr]));
+          frequencyList.push("A5");
           break;
         case '>':
           ptr++;
           tape[ptr] = tape[ptr] || 0;
+          frequencyList.push("E5");
           break;
         case '<':
           ptr--;
           tape[ptr] = tape[ptr] || 0;
+          frequencyList.push("D5");
           break;
         case '[':
           tape[ptr] === 0 
             ? isLooping = true
             : loopStack.push(i);
+          frequencyList.push("C5");
           break;
         case ']':
           tape[ptr] !== 0
             ? i = loopStack[loopStack.length-1]
             : loopStack.pop();
+          frequencyList.push("C6");
           break;
         default:
           break;
-        }
       }
+    }
+
+    return frequencyList
   }
